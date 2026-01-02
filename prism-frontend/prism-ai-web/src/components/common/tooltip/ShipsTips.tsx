e643f9ea53e01f501beada48f51c121cd04ad5a2
JDJCREATES <jdj.creates@gmail.com>
2026-01-02 06:30:48 -0600

    update ShipsTips tooltip

--- prism-frontend/prism-ai-web/src/components/common/tooltip/ShipsTips.tsx
+++ prism-frontend/prism-ai-web/src/components/common/tooltip/ShipsTips.tsx
@@ -1,4 +1,4 @@
-
+import './ShipsTips.css'
 
 export interface tipsTypes {
   
@@ -7,25 +7,42 @@ export interface tipsTypes {
   description: string;
   direction: string;
   
+  tipsType?: 'info' | 'warning' | 'demo' | 'feature';
   apiDocLink?: string;
-  
+  gifLink?: string: // local path to relevant gif
+  gifAlt?: string: //alternaye text for gif/image
 }
 
-const ShipsTips = ({tipId, title, description, direction, apiDocLink}: tipsTypes) => {
+const ShipsTips = ({ tip }: {tip: tipsTypes}) => {
   
   return (
     <>
-      <ons-popover className="ships-tips-container" id={tipId} direction={direction}>
+      <ons-popover 
+        className="ships-tips-container" 
+        id={tip.tipId} 
+        direction={tip.direction}
+        modifier= "tooltip"
+        cancelablel
+      >
         
         <span className="tip-title">
-          {title}
+          {tip.title}
         </span>
         
         <div className="tip-description">
-          {description}
+          {tip.description}
           <div className="api-link">
-            {apiDocLink}
+            {tip.apiDocLink}
           </div>
+          {tip.gifLink && (
+            <div className="gif-section">
+              <img
+                src={tip.gifLink}
+                alt={gifAlt}
+              />
+            </div>
+          )}
+
         </div>
         
       </ons-popover>
